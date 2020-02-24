8decadd8e48eb36913f146eaa5af10bbc55fe29d
aqibalvaaiziin <aqibalvaaiziin@gmail.com>
2020-02-24 15:04:50 +0700

    kurang

--- app/src/main/java/id/putraprima/skorbola/MatchActivity.java
+++ app/src/main/java/id/putraprima/skorbola/MatchActivity.java
@@ -2,17 +2,69 @@ package id.putraprima.skorbola;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.io.IOException;
 
 public class MatchActivity extends AppCompatActivity {
+    TextView homeName;
+    TextView awayName;
+    ImageView homeLogo;
+    ImageView awayLogo;
+    Uri uri1;
+    Uri uri2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_match);
+        homeName = findViewById(R.id.txt_home);
+        awayName = findViewById(R.id.txt_away);
+        homeLogo = findViewById(R.id.home_logo);
+        awayLogo = findViewById(R.id.away_logo);
+
         //TODO
         //1.Menampilkan detail match sesuai data dari main activity
         //2.Tombol add score menambahkan satu angka dari angka 0, setiap kali di tekan
         //3.Tombol Cek Result menghitung pemenang dari kedua tim dan mengirim nama pemenang ke ResultActivity, jika seri di kirim text "Draw"
+        Bundle extras = getIntent().getExtras();
+        String homeTeam = extras.getString("inputHome");
+        String awayTeam = extras.getString("inputAway");
+        String homeImage = extras.getString("logoHome");
+        String awayImage = extras.getString("logoAway");
+
+        if (extras != null) {
+            // TODO: display value here
+
+            uri1 = Uri.parse(extras.getString("logoHome"));
+            uri2 = Uri.parse(extras.getString("logoAway"));
+            Bitmap bitmap1 = null;
+            Bitmap bitmap2 = null;
+
+            try {
+                bitmap1 = MediaStore.Images.Media.getBitmap(this.getContentResolver(), uri1);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            try {
+                bitmap2 = MediaStore.Images.Media.getBitmap(this.getContentResolver(), uri2);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            homeName.setText(homeTeam);
+            awayName.setText(awayTeam);
+            homeLogo.setImageBitmap(bitmap1);
+            awayLogo.setImageBitmap(bitmap2);
+
+        }
     }
+
+
 }
